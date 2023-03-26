dd56be1caea760f0a65fd4acbc15ee9bbe313bf4
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-03-26 22:23:51 +0300

    add: error handlers

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sys
 from flask import Flask, redirect, url_for, render_template
+from werkzeug.exceptions import HTTPException
+
 
 app = Flask("bookhmsrvr")
 app.secret_key = sys.argv[1].encode()
@@ -17,6 +19,17 @@ def favicon():
     return redirect(url_for('favicon.ico'))
 
 
+@app.errorhandler(HTTPException)
+def http_error_handler(error):
+    return render_template("error.html", error=error), error.code
+
+
+@app.errorhandler(Exception)
+def other_error_handler(error):
+    return render_template("error.html", error=error)
+
+
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="127.0.0.1", port=80)
+    serve(app, host="127.0.0.1", port=8080)
+
