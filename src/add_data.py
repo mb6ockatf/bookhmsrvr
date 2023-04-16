820d3e489f6538973624387b6dd34461e5fc1ece
mb6ockatf <mdddmmmm@yandex.ru>
2023-04-16 23:54:40 +0300

    fix: content adding script

--- src/add_data.py
+++ src/add_data.py
@@ -1,17 +1,13 @@
 #!usr/bin/env python3
 import os
-from pprint import pprint
-from logging import info, error, warning
+from logging import info, error, warning, DEBUG
 from functions import configuration, Logger
 from database import load_queries, DatabaseConnection
 
 
-def add_data(debug: bool):
-    logger = Logger(filename="add_data.log", logging_level=debug)
-    if debug:
-        config = "example_data.ini"
-    else:
-        config = "data.ini"
+def add_data():
+    logger = Logger(filename="add_data.log", logging_level=DEBUG)
+    config = "data.ini"
     config = configuration(config)
     database_config = configuration("config.ini")["postgresql"]
     queries_manager = load_queries()
@@ -19,138 +15,158 @@ def add_data(debug: bool):
     for element in config.sections():
         if element == "author":
             try:
-                query_data = {"name": config[element]["name"]}
+                query_data = [config[element]["name"],]
             except KeyError as e:
                 error("minimal author data is not provided")
                 raise e
             try:
                 birthday = config[element]["birthday"]
-                query_data["birthday"] = birthday
+                if birthday == "":
+                    query_data.append("01.01.0001")
+                else:
+                    query_data.append(birthday)
                 info("birthday loaded")
             except KeyError:
-                query_data["birthday"] = None
+                query_data.append("01.01.0001")
                 warning("author's birthday is not loaded")
             try:
                 nationality = config[element]["nationality"]
-                query_data["nationality"] = nationality
+                if nationality == "":
+                    query_data.append(None)
+                else:
+                    query_data.append(nationality)
                 info("nationality loaded")
             except KeyError:
-                query_data["nationality"] = None
+                query_data.append(None)
                 warning("author's nationality is not loaded")
+            query_data.append(1)  # viewscounter
             try:
                 description_file = config[element]["description"]
                 with open(description_file, encoding="utf-8") as file:
                     description = file.read()
-                query_data["description"] = description
+                query_data.append(description)
                 info("author description loaded")
             except KeyError:
-                query_data["description"] = None
+                print("keyerror in desc")
+                query_data.append(None)
                 warning("author's description is not configured")
             except FileNotFoundError:
-                query_data["description"] = None
+                print("fileerror in desc")
+                query_data.append(None)
                 error("provided author's description file does not exist")
             try:
                 wikilink = config[element]["wikilink"]
-                query_data["wikilink"] = wikilink
+                if wikilink == "":
+                    query_data.append(None)
+                else:
+                    query_data.append(wikilink)
                 info("author's wikilink loaded")
             except KeyError:
-                query_data["wikilink"] = None
+                query_data.append(None)
                 warning("author's wikilink is not loaded")
+            queries_manager = load_queries()
+            query = queries_manager["add_author"]
+            db_connection.execute_query(query, query_data)
+            info("BOOK ADDED SUCCESSFULLY")
         elif element == "book":
+            print(element)
             try:
-                query_data = {"name": config[element]["name"],
-                              "author": config[element]["author"],
-                              "downloadscounter": 0,
-                              "viewerscounter": 0,
-                              "rating": None}
-                info("minimal book data loaded")
+                name = config[element]["name"].replace("_", " ")
+                author = config[element]["author"].replace("_", " ")
+                query_data = [name, author,]
+                info("name and author loaded")
             except KeyError as e:
                 error("minimal book data is not provided")
                 raise e
             try:
                 duedate = config[element]["duedate"]
-                query_data["duedate"] = duedate
+                if duedate == "":
+                    query_data.append("01.01.0001")
+                else:
+                    query_data.append(duedate)
                 info("book date data loaded")
             except KeyError:
-                query_data["duedate"] = None
+                query_data.append("01.01.0001")
                 warning("book date is not configured")
             try:
                 language = config[element]["language"]
-                query_data["language"] = language
+                query_data.append(language)
                 info("book language loaded")
             except KeyError:
-                query_data["language"] = None
+                query_data.append(None)
                 warning("book language is not configured")
             try:
                 pages_number = config[element]["pages"]
-                query_data["pages"] = int(pages_number)
+                query_data.append(int(pages_number))
                 info("book pages number loaded")
             except KeyError:
-                query_data["pages"] = None
+                query_data.append(None)
                 warning("book pages number is not configured")
             try:
                 genre = config[element]["genre"]
-                query_data["genre"] = genre
+                query_data.append(genre)
                 info("book genre loaded")
             except KeyError:
-                query_data["genre"] = None
+                query_data.append(None)
                 warning("book genre is not configured")
             try:
                 description_file = config[element]["description"]
                 with open(description_file, encoding="utf-8") as file:
                     description = file.read()
-                query_data["description"] = description
+                query_data.append(description)
                 info("book description loaded")
             except KeyError:
-                query_data["description"] = None
+                query_data.append(None)
                 warning("book description is not configured")
             except FileNotFoundError:
-                query_data["description"] = None
+                query_data.append(None)
                 error("provided book description file does not exist")
             try:
                 wikilink = config[element]["wikilink"]
-                query_data["wikilink"] = wikilink
+                query_data.append(wikilink)
                 info("book wikilink loaded")
             except KeyError:
-                query_data["wikilink"] = None
+                query_data.append(None)
                 warning("book wiki link is not configured")
+            query_data.append(0)  # rating
+            query_data.append(1)  # viewerscounter
+            query_data.append(1)  # downloadscounter
             try:
                 epub_file = config[element]["epub"]
                 with open(epub_file, "rb") as bytestream:
                     contents = bytestream.read()
-                    query_data["epub"] = contents  
+                    query_data.append(contents)
                 info("book epub file loaded")
-                query_data["epubsize"] = len(contents)
+                query_data.append(len(contents))
                 info("book eoub file size loaded")
             except KeyError:
-                query_data["epub"] = None
-                query_data["epubsize"] = None
+                query_data.appned([None, None])
                 warning("book epub file is not configured")
             except FileNotFoundError:
-                query_data["epub"] = None
-                query_data["epubsize"] = None
+                for _ in range(2):
+                    query_data.append(None)
                 info("provided epub file does not exist")
             try:
                 pdf_file = config[element]["pdf"]
                 with open(pdf_file, "rb") as bytestream:
                     contents = bytestream.read()
-                    print(type(contents))
-                    query_data["pdf"] = contents
+                    query_data.append(contents)
                 info("book pdf file loaded")
-                query_data["pdfsize"] = len(contents)
+                query_data.append(len(contents))
                 info("book pdf file size loaded")
             except KeyError:
-                query_data["pdf"] = None
-                query_data["pdfsize"] = None
+                for _ in range(2):
+                    query_data.append(None)
                 warning("book pdf file is not configured")
             except FileNotFoundError:
-                query_data["pdf"] = None
-                query_data["pdfsize"] = None
+                for _ in range(2):
+                    query_data.append(None)
                 info("provided pdf file does not exist")
             queries_manager = load_queries()
             query = queries_manager["add_book"]
             db_connection.execute_query(query, query_data)
+            info("BOOK ADDED SUCCESSFULLY")
 
 
 if __name__ == "__main__":
-    add_data(debug=True)
+    add_data()
