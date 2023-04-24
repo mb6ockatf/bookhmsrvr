cdaa3596c33de64a4b8e62b6733057095731182d
mb6ockatf <mdddmmmm@yandex.ru>
2023-04-24 18:03:44 +0300

    many stuff (#22)
    * chore(CHANGELOG.md): deleted as outdated
    
    * temporary
    
    * add: restapi

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 from sys import argv
+import os.path
+from os import listdir, remove
+from time import sleep
+from threading import Thread
 from logging import WARNING
 from dataclasses import dataclass
 from datetime import datetime
-from pprint import pprint
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, escape
+from flask import send_file
 from werkzeug.exceptions import HTTPException
 from waitress import serve
 from functions import configuration, Logger
@@ -12,8 +16,26 @@ from database import load_queries, DatabaseConnection
 from add_data import add_data
 
 
+def cleaner():
+    """clear cached files"""
+    while True:
+        counter = 0
+        for file in os.listdir("."):
+            if os.path.isfile(file):
+                if file.endswith(".epub") or file.endswith(".pdf"):
+                    counter += 1
+                    remove(file)
+        sleep(3600)
+
+
 def get_safe_url(url: str) -> str:
     return url.replace(" ", "_")
+
+
+def interpret_text(text: str) -> str:
+    text = str(escape(text))
+    text = text.replace("\n", "<br>")
+    return text
 
 
 @dataclass
@@ -80,9 +102,26 @@ def index():
     return render_template("index.html", top_book_list=books_list)
 
 
+@app.route("/authors/<author>")
+def author(author: str):
+    """return author page"""
+    data = (author,)
+    query = queries_manager["select_author"]
+    result = db_connection.execute_query(query, data)
+    template_data = dict()
+    template_data["author"] = author
+    template_data["birthday"] = result[1]
+    template_data["nationality"] = result[2]
+    template_data["viewscounter"] = result[3]
+    template_data["description"] = interpret_text(result[4])
+    template_data["wikilink"] = result[5]
+    return render_template("author.html", **template_data)
+
+
 @app.route('/books/<author>/<book>')
 def book(author: str, book: str):
     """return book page"""
+    splitter = "<br>"
     query = queries_manager["select_book"]
     book, author = book.replace("_", " "), author.replace("_", " ")
     query_data = (book, author)
@@ -93,14 +132,38 @@ def book(author: str, book: str):
     template_data["duedate"] = result[1]
     template_data["language"] = result[2]
     template_data["pages"] = result[3]
-    template_data["description"] = result[4]
+    template_data["description"] = interpret_text(result[4])
     template_data["wikilink"] = result[5]
     template_data["rating"] = result[6]
     template_data["viewerscounter"] = result[7]
     template_data["downloadscounter"] = result[8]
-    template_data["epubsize"] = result[9]
-    template_data["pdfsize"] = result[10]
+    template_data["epubsize"] = result[9] if result[9] is not None else 0
+    template_data["pdfsize"] = result[10] if result[10] is not None else 0
+    query = queries_manager["increment_book_view"]
+    db_connection.execute_query(query, query_data)
     return render_template("book.html", **template_data)
+
+
+@app.route("/download/<author>/<book>", methods=["GET"])
+def download(author: str, book: str):
+    """download files from database"""
+    if request.method == "POST":
+        return 404
+    format = request.args.get("format")
+    data = (author, book)
+    book_name = author + "-" + book
+    if format == "epub":
+        book_name += ".epub"
+        query = queries_manager["select_book_epub"]
+    else:
+        book_name += ".pdf"
+        query = queries_manager["select_book_pdf"]
+    if os.path.isfile(book_name):
+        return send_file(book_name, as_attachment=True)
+    blob = db_connection.execute_query(query, data)
+    open("src/" + book_name, "wb").write(blob[0][0])
+    return send_file(book_name, as_attachment=True)
+
 
 
 @app.route("/favicon.ico")
@@ -108,18 +171,18 @@ def favicon():
     """favicon picture"""
     return redirect(url_for("favicon.ico"))
 
-
+"""
 @app.errorhandler(HTTPException)
 def http_error_handler(error):
     '''http error page'''
-    return render_template("error.html", error=error), error.code
+    return render_template("error.html", error=str(error)), error.code
 
 
 @app.errorhandler(Exception)
 def other_error_handler(error):
     '''system error handler. returns internal server error http code'''
-    return render_template("error.html", error=error), 500
-
+    return render_template("error.html", error=str(error)), 500
+"""
 
 if DEBUG:
     app.run(debug=True)
